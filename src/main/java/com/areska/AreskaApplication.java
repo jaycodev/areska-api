c7a21d5734ad4539a8f5ec7d424ab6592d444862
jaycodev <jason.vilac@gmail.com>
2025-09-29 23:48:37 -0500

    ✨ feat(init): initialize Spring Boot project with base structure

---
+++ src/main/java/com/areska/AreskaApplication.java
@@ -0,0 +1,13 @@
+package com.areska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AreskaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AreskaApplication.class, args);
+	}
+
+}
