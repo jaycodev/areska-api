247a6cec25fb928ec58cec8a91db977ba9f1017f
jaycodev <jason.vilac@gmail.com>
2025-10-06 18:33:55 -0500

    📝 docs(api): add documentation configuration with springdoc-openapi

--- src/main/java/com/areska/AreskaApplication.java
+++ src/main/java/com/areska/AreskaApplication.java
@@ -3,11 +3,21 @@ package com.areska;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AreskaApplication {
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+@SpringBootApplication
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Areska API",
+        version = "1.0.0",
+        description = "Backend API for Areska e-commerce platform, providing services for product management.",
+        license = @License(name = "MIT", url = "https://opensource.org/licenses/MIT")
+    )
+)
+public class AreskaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AreskaApplication.class, args);
 	}
-
 }
