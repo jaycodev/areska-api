a37cf4ee5d86c3c26813b47135cd0abe505f333d
Claudio Angelo Chumpitaz Flores <99515221+XDRiderXtremeXD@users.noreply.github.com>
2025-10-08 23:13:21 -0500

    ✨ feat(user): add domain with dto, entity, repository, service, and controller (pending service and controller logic) (#15)

---
+++ src/main/java/com/areska/user/User.java
@@ -0,0 +1,42 @@
+package com.areska.user;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer userId;
+	
+	@Column(nullable = false)
+	private String firstName;
+	
+	@Column(nullable = false)
+	private String lastName;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(nullable = false)
+	private String phone;
+	
+	private String address;
+	
+	@Column(insertable = false, updatable = false)
+	private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/areska/user/UserController.java
@@ -0,0 +1,5 @@
+package com.areska.user;
+
+public class UserController {
+
+}
